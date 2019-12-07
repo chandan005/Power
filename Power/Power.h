406969cfcedb675fafcaadfcfe06ab77eaeaac1e
chandansingh <chandan.singh@brauz.com>
2019-12-07 22:14:31 +1100

    Initial Commit

---
+++ Power/Power.h
@@ -0,0 +1,19 @@
+//
+//  Power.h
+//  Power
+//
+//  Created by Chandan Singh on 12/7/19.
+//  Copyright © 2019 Developer005. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Power.
+FOUNDATION_EXPORT double PowerVersionNumber;
+
+//! Project version string for Power.
+FOUNDATION_EXPORT const unsigned char PowerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Power/PublicHeader.h>
+
+
